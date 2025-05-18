ab5a366c9173cd991df8520c4347ae8d1db3f233
Mariana1508 <mariana.fernanda.oliveira@escola.pr.gov.br>
2025-05-17 23:29:46 -0300

    Update sistema-livraria.sql

--- sistema-livraria.sql
+++ sistema-livraria.sql
@@ -13,3 +13,19 @@ CREATE TABLE Compras (
     NomeLivro TEXT NOT NULL,
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
 );
+
+SELECT * FROM Clientes; 
+
+-- Inserindo dados na tabela Clientes
+INSERT INTO Clientes (nomeCliente, emailCliente) VALUES
+    ('Mariana Silva', 'mariana.silva@email.com'),
+    ('João Pereira', 'joao.pereira@email.com'),
+    ('Ana Souza', 'ana.souza@email.com');
+
+-- Inserindo dados na tabela Compras
+INSERT INTO Compras (ClienteID, NomeLivro) VALUES
+    (1, 'Dom Quixote'),
+    (2, 'O Pequeno Príncipe'),
+    (3, '1984'),
+    (1, 'Orgulho e Preconceito'),
+    (2, 'Harry Potter e a Pedra Filosofal');
